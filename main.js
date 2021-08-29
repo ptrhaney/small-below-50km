d5ede5464f28b56cf2f36ec22460dc6f4c111525
ptrhaney <84342049+ptrhaney@users.noreply.github.com>
2021-08-29 15:30:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ require([
 
   // define the earthquakes layer
   const earthquakeLayer = new CSVLayer({
-    url: "./earthquake_data.csv",
+    url: "./small below 50km 10yrs.csv",
     elevationInfo: exaggeratedElevation,
     screenSizePerspectiveEnabled: false,
     renderer: {
